32c3c11eb40ba582437d854e53d8356075f39d4d
DavidHiuhu <156612647+DavidHiuhu@users.noreply.github.com>
2024-03-08 22:35:12 -0500

    Add files via upload
    Create resource with dependencies (implicit and explicit).

--- main.tf
+++ main.tf
@@ -10,26 +10,45 @@ terraform {
 provider "docker" {
 }
 
+# Create an Nginx Docker image
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
 
+# Create an Nginx Docker container with explicit dependency on the Nginx image
 resource "docker_container" "nginx" {
   name  = "nginx"
-  image = docker_image.nginx.name 
+  image = docker_image.nginx.name
+
   ports {
     internal = 80
     external = 8081
   }
 }
 
+# Create an Ubuntu Docker image
 resource "docker_image" "ubuntu" {
   name = "ubuntu:latest"
 }
 
+# Create an Ubuntu Docker container with explicit dependency on the Ubuntu image
 resource "docker_container" "ubuntu" {
   name  = "ubuntu"
-  image = docker_image.ubuntu.name 
+  image = docker_image.ubuntu.name
 
   command = ["/bin/bash", "-c", "tail -f /dev/null"]
 }
+
+# Implicit dependency: Ubuntu container depends on Nginx container
+resource "docker_container" "ubuntu_with_dependency" {
+  name  = "ubuntu_with_dependency"
+  image = docker_image.ubuntu.name
+
+  command = ["/bin/bash", "-c", "tail -f /dev/null"]
+
+  # Implicit dependency on the Nginx container
+  depends_on = [
+    docker_container.nginx,
+  ]
+}
+
